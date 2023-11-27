59f30ba6e17d55cb2615ae448f87660ca2d6fc86
oumi youss <oumiyouss@hotmail.fr>
2023-11-27 22:56:34 +0100

    test calendar , warning: code calendar in dashboardjs

--- front/src/components/screens/Generic/Calendar.js
+++ front/src/components/screens/Generic/Calendar.js
@@ -1,40 +1,111 @@
-import { StatusBar, StyleSheet, View, ScrollView, Text} from 'react-native';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faBell , faCalendarDays, faUser} from '@fortawesome/free-regular-svg-icons';
+import React, { useState } from 'react';
+// import all the components we are going to use
+import { SafeAreaView, StyleSheet, View, Text, Button } from 'react-native';
+//import CalendarPicker from the package we installed
+import CalendarPicker from 'react-native-calendar-picker';
+// import axios from 'axios';
 
-export default function Calendar() {
+const Calendar = () => {
+  const [selectedStartDate, setSelectedStartDate] = useState(null);
+  const [selectedEndDate, setSelectedEndDate] = useState(null);
 
+  const onDateChange = (date, type) => {
+    //function to handle the date change
+    if (type === 'END_DATE') {
+      setSelectedEndDate(date);
+    } else {
+      setSelectedEndDate(null);
+      setSelectedStartDate(date);
+    }
+  };
+  const onPress = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/phases/656352da6200d8f968a83d7b', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ startDate: selectedStartDate, endDate : selectedEndDate }),
+      });
+
+      if (response.ok) {
+        console.log('Date enregistrée avec succès');
+      } else {
+        console.log('Erreur lors de l\'enregistrement de la date');
+      }
+    } catch (error) {
+      console.log('Erreur lors de l\'enregistrement de la date:', error);
+    }
+ };
   return (
-    <View style={styles.container}>
-      <StatusBar></StatusBar>
-      <ScrollView></ScrollView>
-        {/* <View style={[styles.homeHeader]}>
-          <FontAwesomeIcon icon={faBell} />
-          <View style={{flexDirection: 'row'}}>
-            <Text> Month 2023</Text>
-            <FontAwesomeIcon icon={faCalendarDays} />
-          </View>
-          <FontAwesomeIcon icon={faUser} />
-        </View> */}
-        <Text>Hello world</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Text style={styles.titleStyle}>React Native Calendar Picker</Text>
+        <CalendarPicker
+          startFromMonday={true}
+          allowRangeSelection={true}
+          minDate={new Date(2018, 1, 1)}
+          maxDate={new Date(2050, 6, 3)}
+          weekdays={['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun']}
+          months={[
+            'January',
+            'Febraury',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December',
+          ]}
+          previousTitle="Previous"
+          nextTitle="Next"
+          todayBackgroundColor="#e6ffe6"
+          selectedDayColor="#66ff33"
+          selectedDayTextColor="#000000"
+          scaleFactor={375}
+          textStyle={{
+            color: '#000000',
+          }}
+          onDateChange={onDateChange}
+        />
+        <Button 
+          onPress={onPress}
+          title='Enregistrer'>
+
+        </Button>
+        <View style={styles.textStyle}>
+          <Text style={styles.textStyle}>Selected Start Date :</Text>
+          <Text style={styles.textStyle}>
+            {selectedStartDate ? selectedStartDate.toString() : ''}
+          </Text>
+          <Text style={styles.textStyle}>Selected End Date : </Text>
+          <Text style={styles.textStyle}>
+            {selectedEndDate ? selectedEndDate.toString() : ''}
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
-}
+};
+export default Calendar;
 
 const styles = StyleSheet.create({
   container: {
-    
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: 40,
+    flex: 1,
+    paddingTop: 30,
+    backgroundColor: '#ffffff',
+    padding: 16,
   },
-  homeHeader: {
-    backgroundColor: "pink",
-    height: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-  }
+  textStyle: {
+    marginTop: 10,
+  },
+  titleStyle: {
+    textAlign: 'center',
+    fontSize: 20,
+    margin: 20,
+  },
 });
-
-
--- front/src/components/screens/Generic/Dashboard.js
+++ front/src/components/screens/Generic/Dashboard.js
@@ -1,17 +1,203 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context';
-import React from 'react'
-import BottomTabs from '../../common/BottomTabs'
+// import { StyleSheet, Text, View } from 'react-native'
+// import { SafeAreaView } from 'react-native-safe-area-context';
+// import React from 'react'
+// import BottomTabs from '../../common/BottomTabs'
 
-export default function Dashboard() {
+// export default function Dashboard() {
+//   return (
+//     <SafeAreaView>
+//       <View>
+//         <Text>Dashboard</Text>
+//         <BottomTabs />
+//       </View>
+//     </SafeAreaView>
+//   )
+// }
+
+// const styles = StyleSheet.create({})
+import React, { useState } from 'react';
+// import all the components we are going to use
+import { SafeAreaView, StyleSheet, View, Text, Button } from 'react-native';
+//import CalendarPicker from the package we installed
+import CalendarPicker from 'react-native-calendar-picker';
+import axios from 'axios';
+
+// const Dashboard = () => {
+//   const [selectedStartDate, setSelectedStartDate] = useState(null);
+//   const [selectedEndDate, setSelectedEndDate] = useState(null);
+
+//   const onDateChange = (date, type) => {
+//     //function to handle the date change
+//     if (type === 'END_DATE') {
+//       setSelectedEndDate(date);
+//     } else {
+//       setSelectedEndDate(null);
+//       setSelectedStartDate(date);
+//     }
+//   };
+//   const onPress = async () => {
+//     try {
+//       const response = await fetch('http://localhost:3000/phases/656352da6200d8f968a83d7b', {
+//         method: 'PUT',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ startDate: selectedStartDate, endDate : selectedEndDate }),
+//       });
+
+//       if (response.ok) {
+//         console.log('Date enregistrée avec succès');
+//       } else {
+//         console.log('Erreur lors de l\'enregistrement de la date');
+//       }
+//     } catch (error) {
+//       console.log('Erreur lors de l\'enregistrement de la date:', error);
+//     }
+//  };
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <View style={styles.container}>
+//         <Text style={styles.titleStyle}>React Native Calendar Picker</Text>
+//         <CalendarPicker
+//           startFromMonday={true}
+//           allowRangeSelection={true}
+//           minDate={new Date(2018, 1, 1)}
+//           maxDate={new Date(2050, 6, 3)}
+//           weekdays={['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun']}
+//           months={[
+//             'January',
+//             'Febraury',
+//             'March',
+//             'April',
+//             'May',
+//             'June',
+//             'July',
+//             'August',
+//             'September',
+//             'October',
+//             'November',
+//             'December',
+//           ]}
+//           previousTitle="Previous"
+//           nextTitle="Next"
+//           todayBackgroundColor="#e6ffe6"
+//           selectedDayColor="#66ff33"
+//           selectedDayTextColor="#000000"
+//           scaleFactor={375}
+//           textStyle={{
+//             color: '#000000',
+//           }}
+//           onDateChange={onDateChange}
+//         />
+//         <Button 
+//           onPress={onPress}
+//           title='Enregistrer'>
+
+//         </Button>
+//         <View style={styles.textStyle}>
+//           <Text style={styles.textStyle}>Selected Start Date :</Text>
+//           <Text style={styles.textStyle}>
+//             {selectedStartDate ? selectedStartDate.toString() : ''}
+//           </Text>
+//           <Text style={styles.textStyle}>Selected End Date : </Text>
+//           <Text style={styles.textStyle}>
+//             {selectedEndDate ? selectedEndDate.toString() : ''}
+//           </Text>
+//         </View>
+//       </View>
+//     </SafeAreaView>
+//   );
+// };
+const Dashboard = () => {
+  const [selectedStartDate, setSelectedStartDate] = useState(null);
+  const [selectedEndDate, setSelectedEndDate] = useState(null);
+
+
+  const onDateChange = (date, type) => {
+    //function to handle the date change
+    if (type === 'END_DATE') {
+      setSelectedEndDate(date);
+    } else {
+      setSelectedEndDate(null);
+      setSelectedStartDate(date);
+    }
+  };
+
+  const handleSaveDates = () => {
+    const category = 'Menses Phase';
+    saveDates(selectedStartDate, selectedEndDate, category)
+  };
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Dashboard</Text>
-        <BottomTabs />
-      </View>
-    </SafeAreaView>
-  )
-}
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Text style={styles.titleStyle}>React Native Calendar Picker</Text>
+        <CalendarPicker
+          startFromMonday={true}
+          allowRangeSelection={true}
+          minDate={new Date(2018, 1, 1)}
+          maxDate={new Date(2050, 6, 3)}
+          weekdays={['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun']}
+          months={[
+            'January',
+            'Febraury',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December',
+          ]}
+          previousTitle="Previous"
+          nextTitle="Next"
+          todayBackgroundColor="#e6ffe6"
+          selectedDayColor="#66ff33"
+          selectedDayTextColor="#000000"
+          scaleFactor={375}
+          textStyle={{
+            color: '#000000',
+          }}
+          onDateChange={onDateChange}
+        />
+    
+  <Button 
+        title="Save the dates"
+        onPress={handleSaveDates}
+        >
+</Button>
+<View style={styles.textStyle}>
+           <Text style={styles.textStyle}>Selected Start Date :</Text>
+           <Text style={styles.textStyle}>
+             {selectedStartDate ? selectedStartDate.toString() : ''}
+           </Text>
+           <Text style={styles.textStyle}>Selected End Date : </Text>
+           <Text style={styles.textStyle}>
+             {selectedEndDate ? selectedEndDate.toString() : ''}
+           </Text>
+         </View>
+       </View>
+     </SafeAreaView>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default Dashboard;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 30,
+    backgroundColor: '#ffffff',
+    padding: 16,
+  },
+  textStyle: {
+    marginTop: 10,
+  },
+  titleStyle: {
+    textAlign: 'center',
+    fontSize: 20,
+    margin: 20,
+  },
+});
